7fa820761af607a2333b0885818b044f42a87dbb
Your Name <dangelo.watson1212@gmail.com>
2023-02-22 10:39:06 -0500

    finished with counte component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import './ReactCounter.css';
+import { useState } from 'react'
+
+
+function ReactCounter(){
+
+  const [count, setCount] = useState(0);
+
+  function decrease(){
+    setCount(count - 1)
+  }
+
+  function increase(){
+    setCount(count + 1)
+  }
+
+  function reset(){
+    setCount(0)
+  }
+
+  return(
+    <div className='ReactCounter'>
+      <div className='Counter-section'>
+        <h1>React Counter</h1>
+
+        <p> Your Current Count is</p>
+
+        <div className='Counter'>
+          <p>{count}</p>
+        </div>
+
+        <div className='buttons'>
+          <button onClick={decrease} className='minus'>-</button>
+          <button onClick={reset} className='reset'>Reset</button>
+          <button onClick={increase} className='add'>+</button>
+
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactCounter /> 
     </div>
   );
 }
 
 export default App;
+
